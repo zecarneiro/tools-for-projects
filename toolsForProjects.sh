17b26551b3c99ba02cb020c37accc3fc9c8a8342
José M. C. Noronha <jose@jose-HP-ENVY-Sleekbook-6-PC>
2019-04-08 18:07:06 +0100

    Update virtualConfNginxTemplate, virtualConfApacheTemplate.conf, and toolsForProjects.sh

--- toolsForProjects.sh
+++ toolsForProjects.sh
@@ -6,6 +6,7 @@ declare nameProjectArray=("Install Dendencies" "Angular" "CakePHP" "Docker" "Dat
 declare currentPath="$(echo $PWD)"
 declare pid
 declare isKillPID="0"
+declare filesTemplatePath="/opt/tools_for_projects/files"
 
 ################################################
 # Generic
@@ -295,18 +296,13 @@ function cakePhpOther () {
 function setCakeBashCompletion () {
     echo "Set Cake Bash Completion..."
     local _file_cake_script="/etc/bash_completion.d/cake"
-    local cake_bash_completion_directory="/opt/tools_for_projects/files/"
 
     # Install bash completion
     sudo apt install bash-completion -y
 
     # Install script
-    if [ -d "$cake_bash_completion_directory" ]; then
-        if [ -f "$_file_cake_script" ]; then
-            sudo cp "$cake_bash_completion_directory" "$_file_cake_script"
-        else
-            echo "Error on set bash cake complete"
-        fi
+    if [ -f "$filesTemplatePath/cake" ]; then
+       sudo cp "$filesTemplatePath/cake" "$_file_cake_script"
     else
         echo "Error on set bash cake complete"
     fi
@@ -890,7 +886,6 @@ function getTecnology () {
     echo "$option"
 }
 
-
 function configWebServer () {
     local webserver_selected="$(getWebServer)"
     local tecnology_selected="$(getTecnology)"
@@ -923,6 +918,186 @@ function configWebServer () {
     fi
 }
 
+function enableWebProject () {
+    local webserver_selected="$(getWebServer)"
+    local path_default="$(echo $PWD)"
+
+    # Get path
+    printf "\nPress ENTER to default: $path_default\n"
+    read -p "Insert full path of web root path project: " new_path_project
+    if [ ! -z "$new_path_project" ]&&[ -d "$new_path_project" ]; then
+        path_default="$new_path_project"
+    fi
+    
+    # Get port
+    read -p "Insert port for project( PRESS ENTER TO CANCEL ): " port_project
+
+    # Get name of project
+    read -p "Insert name of project(Whitout space): " name_project
+
+    if [ -n "$port_project" ]&&[ -n "$name_project" ]; then
+        printf "\nInfo inserted:\n"
+        echo "Web Root Path: $path_default"
+        echo "PORT: $port_project"
+        echo "Name Project: $name_project"
+        read -p "Continue?[y/N]: " is_continue
+        echo ""
+
+        if [ "$is_continue" = "y" ]; then
+            is_continue=""
+
+            # Apache
+            if [ "$webserver_selected" = "1" ]; then
+                local group="$(ps aux | egrep '([a|A]pache|[h|H]ttpd)' | awk '{ print $1}' | uniq | tail -1)"
+                local path_apache="/etc/apache2"
+                local sites_available_path="$path_apache/sites-available"
+                local port_apache_file="$path_apache/ports.conf"
+                local line_number_of_first_listen="$(grep -wn "Listen 80" /etc/apache2/ports.conf | cut -f1 -d':')"
+                local is_port_exist="$(cat "$port_apache_file" | grep -w "Listen $port_project")"
+                local template_apache="$filesTemplatePath/virtualConfApacheTemplate.conf"
+                local file_virtual_project="$sites_available_path/$name_project.conf"
+
+                # Check if port exist
+                if [ -n "$is_port_exist" ]; then
+                    echo "Probably already any project using port $port_project"
+                    read -p "Continue?[y/N]: " is_continue
+                else
+                    is_continue="y"
+                fi
+                
+                if [ "$is_continue" = "y" ]; then
+                    echo "Copy Template..."
+                    sudo cp "$template_apache" "$file_virtual_project"
+
+                    echo "Set PORT..."
+                    sudo sed -i "s/PORTO/$port_project/" "$file_virtual_project"
+                    sudo sed -i "$line_number_of_first_listen a\Listen $port_project" "$port_apache_file"
+
+                    echo "Set PATH..."
+                    sudo sed -i "s#FULL_PATH_PROJECT#$path_default#" "$file_virtual_project"
+
+                    echo "Restart Apache"
+                    service apache2 restart
+
+                    # Info
+                    echo "Considering set group and permission on ROOT Project with this command if necessary:"
+                    printf "\t- sudo chown -R :$group ROOT_PROJECT\n"
+                    printf "\t- sudo chmod -R 755 ROOT_PROJECT\n\n"
+                fi
+
+            # NGinx
+            elif [ "$webserver_selected" = "2" ]; then
+                local group="$(ps aux | egrep '([a|A]pache|[h|H]ttpd)' | awk '{ print $1}' | uniq | tail -1)"
+                local path_nginx="/etc/nginx"
+                local sites_available_path="$path_nginx/sites-available"
+                local sites_enabled_path="$path_nginx/sites-enabled"
+                local is_port_exist="$(sudo lsof -i -n | grep nginx | awk '{print $9}' | cut -d ':' -f2 | grep -c "$port_project")"
+                local template_nginx="$filesTemplatePath"
+                local file_virtual_project="$sites_available_path/$name_project"
+
+                # Check if port exist
+                if [ -n "$is_port_exist" ]; then
+                    echo "Probably already any project using port $port_project"
+                    read -p "Continue?[y/N]: " is_continue
+                else
+                    is_continue="y"
+                fi
+                
+                if [ "$is_continue" = "y" ]; then
+                    local tecnology_selected="$(getTecnology)"
+                    
+                    if [ "$tecnology_selected" = "1" ]; then
+                        template_nginx="$template_nginx/PHP/virtualConfNginxTemplate"
+
+                        echo "Copy Template..."
+                        sudo cp "$template_nginx" "$file_virtual_project"
+
+                        echo "Set PORT..."
+                        sudo sed -i "s/PORTO/$port_project/" "$file_virtual_project"
+
+                        echo "Set PATH..."
+                        sudo sed -i "s#FULL_PATH_PROJECT#$path_default#" "$file_virtual_project"
+
+                        echo "Set PHP Version..."
+                        sudo sed -i "s#PHPVERSION#$phpVersion#" "$file_virtual_project"
+                    fi
+
+                    echo "Set web site..."
+                    sudo ln -s "$file_virtual_project" "$sites_enabled_path"
+
+                    echo "Restart NGinx"
+                    service nginx restart
+
+                    # Info
+                    echo "Considering set group and permission on ROOT Project with this command if necessary:"
+                    printf "\t- sudo chown -R :$group ROOT_PROJECT\n"
+                    printf "\t- sudo chmod -R 755 ROOT_PROJECT\n\n"
+                fi
+            fi
+        fi
+    fi    
+}
+
+function disableWebProject () {
+    local webserver_selected="$(getWebServer)"
+    local path_default="$(echo $PWD)"
+
+    # Get port
+    read -p "Insert port for project( PRESS ENTER TO CANCEL ): " port_project
+
+    # Get name of project
+    read -p "Insert name of project(Whitout space): " name_project
+
+    if [ -n "$port_project" ]&&[ -n "$name_project" ]; then
+        printf "\nInfo inserted:\n"
+        echo "PORT: $port_project"
+        echo "Name Project: $name_project"
+        read -p "Continue?[y/N]: " is_continue
+        echo ""
+
+        if [ "$is_continue" = "y" ]; then
+            # Apache
+            if [ "$webserver_selected" = "1" ]; then
+                local path_apache="/etc/apache2"
+                local sites_available_path="$path_apache/sites-available"
+                local port_apache_file="$path_apache/ports.conf"
+                local line_number_of_listen_port="$(grep -wn "Listen $port_project" /etc/apache2/ports.conf | cut -f1 -d':')"
+                local file_virtual_project="$sites_available_path/$name_project.conf"
+
+                echo "Delete project virtual conf file..."
+                if [ -f "$file_virtual_project" ]; then
+                    sudo rm "$file_virtual_project"
+                fi
+
+                echo "Remove port..."
+                if [ -n "$line_number_of_listen_port" ]; then
+                    sudo sed -i "$line_number_of_listen_port d" "$port_apache_file"
+                fi
+
+                echo "Restart Apache"
+                service apache2 restart
+
+            # NGinx
+            elif [ "$webserver_selected" = "2" ]; then
+                local path_nginx="/etc/nginx"
+                local sites_available_path="$path_nginx/sites-available"
+                local sites_enabled_path="$path_nginx/sites-enabled"
+                local file_virtual_project="$sites_available_path/$name_project"
+
+                echo "Delete project virtual conf file..."
+                if [ -f "$file_virtual_project" ]; then
+                    sudo rm "$file_virtual_project"
+                    sudo rm "$sites_enabled_path/$name_project"
+                fi
+
+                echo "Restart NGinx"
+                service nginx restart
+            fi
+        fi
+    fi
+    printf "\n\n"
+}
+
 function webserverTools () {
     # Clear Screen
     clearScreen
@@ -945,8 +1120,14 @@ function webserverTools () {
         fi
         
         case "$option" in
-            1) # Grant All Access
+            1) # Config Web Servers
                 configWebServer
+            ;;
+            2) # Enable Projects
+                enableWebProject
+            ;;
+            3) # Disable Project
+                disableWebProject
             ;;
             *) # Back
                 break
@@ -954,7 +1135,6 @@ function webserverTools () {
         esac
     done
 }
-
 
 ################################################
 # Dependency Tools
