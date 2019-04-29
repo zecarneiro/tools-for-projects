cef978945146d98b52facddc4360b36a7812b071
José M. C. Noronha <jose@jose-HP-ENVY-Sleekbook-6-PC>
2019-04-29 14:27:26 +0100

    Update toolsForProjects.sh

--- toolsForProjects.sh
+++ toolsForProjects.sh
@@ -1277,37 +1277,91 @@ function convertAllIpOrInterfaceToArray () {
     fi
 }
 
-function getAllIpOnly () {
-    local all_ip="$(hostname -I)"
+function getIpOnly () {
+    local interface="$1"
+    local all_ip="$(ip a show $interface | grep inet | awk -F' ' '{print $2}' | cut -d'/' -f1 | awk '{printf "%s" (NR%3==0?RS:FS),$1}')"
     echo "$all_ip"
 }
 
 function getAllInterfacesOnly () {
-    local all_interfaces="$(ip -o link show | awk -F': ' '{print $2}' | grep -wv lo | awk '{printf "%s" (NR%3==0?RS:FS),$1}')"
+    local all_interfaces="$(ip -o link show | awk -F': ' '{print $2}' | awk '{ORS=" ";}; !NF{ORS="\n"};1')"
     echo "$all_interfaces"
 }
-
 
 function showAllIp () {
     local -i exist
 
     # Get info
-    convertAllIpOrInterfaceToArray "$(getAllIpOnly)" 1
     convertAllIpOrInterfaceToArray "$(getAllInterfacesOnly)" 0
 
     printf "\nList IPs...\n"
 
     for interface in "${all_interface_only[@]}"; do
         echo "INTERFACE: $interface"
+        convertAllIpOrInterfaceToArray "$(getIpOnly "$interface")" 1
         for ip in "${all_ip_only[@]}"; do
             exist="$(ip -f inet addr show "$interface" | grep -wc "$ip")"
-            existSix="$(ip -f inet6 addr show "$interface" | grep -wc "$ip")"
-            if [ "$exist" -gt "0" ]||[ "$existSix" -gt "0" ]; then
-                printf "\t- $ip\n"
+            if [ "$exist" -gt "0" ]; then
+                printf "\t- IPv4: $ip\n"
             fi
+
+            existSix="$(ip -f inet6 addr show "$interface" | grep -wc "$ip")"
+            if [ "$existSix" -gt "0" ]; then
+                printf "\t- IPv6: $ip\n"
+            fi
+            
         done
         printf "\n"
     done
+}
+
+function printListInterface () {
+    # Get info
+    convertAllIpOrInterfaceToArray "$(getAllInterfacesOnly)" 0
+
+    printf "\nList INTERFACES...\n"
+    for interface in "${all_interface_only[@]}"; do
+        printf "\t- $interface\n"
+    done
+}
+
+
+function upInterface () {
+    local interface
+
+    echo "$(printListInterface)"
+    echo
+    read -p "Insert interface(PRESS ENTER TO CANCEL): " interface
+
+    if [ -n "$interface" ]; then
+        sudo ip link set "$interface" up
+    fi
+}
+
+function downInterface () {
+    local interface
+
+    echo "$(printListInterface)"
+    echo
+    read -p "Insert interface(PRESS ENTER TO CANCEL): " interface
+
+    if [ -n "$interface" ]; then
+        sudo ip link set "$interface" down
+    fi
+}
+
+function deleteInterface () {
+    local interface
+
+    echo "$(printListInterface)"
+    
+    printf "\n\nWARNING: THIS ACTION NOT REVERSIBLE\n"
+    read -p "Insert interface(PRESS ENTER TO CANCEL): " interface
+
+    if [ -n "$interface" ]; then
+        sudo ip link set "$interface" down
+        sudo ip link delete "$interface"
+    fi
 }
 
 
@@ -1321,9 +1375,10 @@ function networkTools () {
         echo "0 - Clear Screen"
         echo "------"
         echo "1 - Show All IP"
-        echo "2 - Enable Project"
-        echo "3 - Disable Project"
-        echo "4 - List Enabled Project"
+        echo "2 - UP INTERFACE"
+        echo "3 - DOWN INTERFACE"
+        echo "------"
+        echo "4 - Delete INTERFACE"
         echo "------"
         echo "Back, PRESS ENTER"
         read -p "Insert an option: " option
@@ -1335,14 +1390,14 @@ function networkTools () {
             1) # Show All IP
                 showAllIp
             ;;
-            2) # Enable Projects
-                enableWebProject
+            2) # UP INTERFACE
+                upInterface
             ;;
-            3) # Disable Project
-                disableWebProject
+            3) # DOWN INTERFACE
+                downInterface
             ;;
-            4) # Get List of enabled projects
-                getListEnabledWebProject
+            4) # Delete INTERFACE
+                deleteInterface
             ;;
             *) # Back
                 break
@@ -1351,7 +1406,6 @@ function networkTools () {
         printf "\n\n"
     done
 }
-
 
 ################################################
 # Dependency Tools
