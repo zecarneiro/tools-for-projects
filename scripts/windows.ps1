132caa908c40131376b8c4cd4df340f99284355e
Jos? Noronha <jose.noronha@celfocus.com>
2021-06-07 15:29:56 +0100

    add scr

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -1,7 +1,11 @@
 param(
     [ValidateSet("install","uninstall", IgnoreCase = $false)] # IgnoreCase set autocomplete
     [string]
-    $INSTALLER
+    $INSTALLER,
+    [switch]
+    $RESET_JETBRAINS,
+    [string]
+    $JAVA_PATH = ""
 )
 
 $projectName = "tools-for-projects"
@@ -13,50 +17,83 @@ $listUnnecessaryFiles = @(
     "utils\nodejs-utils\src", "src"
 )
 
+#================================================
+#                FUNCTIONS UTILS
+#================================================
+function WaitForUserInputToConitnue {
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
+function PrintMessage() {
+    param(
+        [ValidateSet("error","info", "warnning", "success", "none", IgnoreCase = $false)] # IgnoreCase set autocomplete
+        [string]
+        $type,
+        [string]
+        $message = ""
+    )
+    if ($type -eq "error") {
+        Write-Host "$message" -ForegroundColor Red
+    } elseif ($type -eq "info") {
+        Write-Host "$message" -ForegroundColor Blue
+    } elseif ($type -eq "warnning") {
+        Write-Host "$message" -ForegroundColor Yellow
+    } elseif ($type -eq "success") {
+        Write-Host "$message" -ForegroundColor Green
+    } else {
+        Write-Host "$message"
+    }
+}
 function InvokeCommand() {
     param (
         [string]$program = $(throw "Please specify a program" ),
         [string]$argumentString = "",
-        [switch]$waitForExit,
         [string]$cwd = ""
     )
-
-    $psi = new-object "Diagnostics.ProcessStartInfo"
-    $psi.FileName = $program 
-    $psi.Arguments = $argumentString
-    if ($cwd.Length -gt 0) {
-        $psi.UseShellExecute = $true
-        $psi.WorkingDirectory = $cwd
-    }
-    $proc = [Diagnostics.Process]::Start($psi)
-    if ( $waitForExit ) {
-        $proc.WaitForExit();
+    Write-Host "`nExecute >>" -ForegroundColor Red -BackgroundColor Yellow
+    PrintMessage -message "`tCOMMAND: $program $argumentString `n`tCWD: $cwd" "info"
+    if (($cwd.Length -gt 0) -and ($argumentString.Length -gt 0)) {
+        Start-Process $program -ArgumentList $argumentString -WorkingDirectory $cwd -NoNewWindow -Wait
+    } elseif (($cwd.Length -gt 0)) {
+        Start-Process $program -WorkingDirectory $cwd -NoNewWindow -Wait
+    } elseif (($argumentString.Length -gt 0)) {
+        Start-Process $program -ArgumentList $argumentString -NoNewWindow -Wait
+    } else {
+        Start-Process $program -NoNewWindow -Wait
     }
 }
-
 function ItemExist {
     param (
         [string]$item,
-        [switch]$isFile
+        [switch]$isFile,
+        [switch]$isRegedit
     )
-    
-    if ($isFile) {
-        return (Test-Path -Path $item -PathType Leaf)
+    if ($item.Length -gt 0) {
+        if ($isRegedit) {
+            $item = "HKCU:\$item"
+        }
+        if ($isFile) {
+            return (Test-Path -Path $item -PathType Leaf)
+        }
+        return (Test-Path $item)
     }
-    return (Test-Path $item)
+    return $false
 }
-
-function RemoveItems {
-    param ([String[]] $items)
-    if ($items.Count -gt 0) {
-        Foreach ($item in $items) {
-            If((ItemExist "$item") -or (ItemExist "$item" -isFile)) {
-                Remove-Item $item -Recurse -Force -ErrorAction Stop
-            }
+function RemoveItem {
+    param (
+        [String]$item,
+        [switch]$isRegedit
+    )
+    if ($item.Length -gt 0) {
+        if ($isRegedit) {
+            $item = "HKCU:\$item"
+        }
+        If((ItemExist "$item") -or (ItemExist "$item" -isFile)) {
+            PrintMessage -message "Delete $item" "none"
+            Remove-Item $item -Recurse -Force -ErrorAction Stop
         }
     }
 }
-
 function CreateItem {
     param (
         [string]$item,
@@ -70,24 +107,30 @@ function CreateItem {
                 } else {
                     $null = New-Item -ItemType Directory -Path "$item" -Force -ErrorAction Stop
                 }
-                Write-Host "The item [$item] has been created."
+                PrintMessage -message "The item [$item] has been created." "success"
             }
             catch {
                 throw $_.Exception.Message
             }
         } else {
-            Write-Host "Cannot create [$item] because a item with that name already exists."
+            PrintMessage -message "Cannot create [$item] because a item with that name already exists." "info"
         }
     } else {
-        Write-Host "Ivalid item given"
+        PrintMessage -message "Ivalid item given" "error"
     }
 }
-
 function CheckCommandExist {
     param ([string]$command)
-    return (where.exe "$command")
+    if ((where.exe "$command")) {
+        return $true
+    }
+    return $false
 }
+#============ END OF FUNCTIONS UTILS ============
 
+#================================================
+#                INSTALL/UNINSTALL
+#================================================
 function Uninstall {
     if (!(CheckCommandExist("node"))) {
         Write-Error -Message "Please install NodeJS"
@@ -96,10 +139,9 @@ function Uninstall {
     if ((CheckCommandExist($projectName))) {
         InvokeCommand -program "npm" -argumentString "uninstall -g $projectName" -waitForExit
     }
-    RemoveItems "$installDir"
-    Write-Host "Uninstall complete"
+    RemoveItem "$installDir"
+    PrintMessage -message "Uninstall complete" "success"
 }
-
 function Install {
     if (!(CheckCommandExist("node"))) {
         Write-Error -Message "Please install NodeJS"
@@ -112,17 +154,87 @@ function Install {
     InvokeCommand -program "npm" -argumentString "install" -waitForExit -cwd "$installDir"
     InvokeCommand -program "npm" -argumentString "run compile" -waitForExit -cwd "$installDir"
     InvokeCommand -program "npm" -argumentString "install -g ." -waitForExit -cwd "$installDir"
-    RemoveItems $listUnnecessaryFiles
+    Foreach ($unnecessaryFile in $listUnnecessaryFiles) {
+        RemoveItem $unnecessaryFile
+    }
     Set-Location "$currentDir"
     Set-Location ..
-    Write-Host "Install complete"
+    PrintMessage -message "Install complete" "success"
+}
+#=========== END OF INSTALL/UNINSTALL ===========
+
+function ResetJetbrains {
+    $baseDir="${env:APPDATA}\JetBrains"
+    $ideInfo = @{
+        idea = @{
+            path = "IntelliJIdea";
+            regedit = "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains\idea"
+        };
+        rider = @{
+            path = "Rider";
+            regedit = ""
+        };
+        clion = @{
+            path = "CLion";
+            regedit = ""
+        };
+        pycharm = @{
+            path = "PyCharm";
+            regedit = ""
+        }
+    }
+    $envDir="eval"
+    $optionsFile="options\other.xml"
+    $ideInfo.GetEnumerator() | ForEach-Object {
+        $allDir=(Get-ChildItem -Path "$baseDir" -Directory -Recurse -Filter "$($_.Value.path + '*')")
+        $regeditPath = "$($_.Value.regedit)"
+        For ($j=0; $j -lt $allDir.Length; $j++) {
+            $directoryEval = ("$baseDir\" + $allDir[$j] + "\$envDir")
+            $otherFile = ("$baseDir\" + $allDir[$j] + "\$optionsFile") 
+            if ((ItemExist "$directoryEval") -or (ItemExist "$otherFile") -or (ItemExist "$regeditPath" -isRegedit)) {
+                PrintMessage -message ("Reset " + $allDir[$j]) "none"
+                RemoveItem "$directoryEval"
+                RemoveItem "$otherFile"
+                RemoveItem "$regeditPath" -isRegedit
+            }
+        }
+    }
+    PrintMessage -message "Done" "success"
 }
 
+function InstallUninstallJava() {
+    $sysenv = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+    $path = (Get-ItemProperty -Path $sysenv -Name Path).Path
+
+    # Remove old
+    $oldJava = "$env:JAVA_HOME"
+    if ($oldJava.Length -gt 0) {
+        PrintMessage -message "Uninstall: $oldJava" "none"
+        $path = $path.replace(";$oldJava\bin", "")
+    }
+
+    if (($JAVA_PATH.Length -gt 0) -and ($JAVA_PATH -ne "-u")) {
+        PrintMessage -message "Install: $JAVA_PATH" "none"
+        setx /m JAVA_HOME "$JAVA_PATH"
+        $path = $path + ";$JAVA_PATH\bin"
+        Set-ItemProperty -Path "$sysenv" -Name Path -Value "$path"
+        Stop-Process -ProcessName explorer
+    }
+}
+
+#================================================
+#                MAIN
+#================================================
 function Main {
     if ($INSTALLER -eq "install") {
         Install
     } elseif ($INSTALLER -eq "uninstall") {
         Uninstall
+    }
+    if ($RESET_JETBRAINS) { ResetJetbrains }
+    if ($JAVA_PATH.Length -gt 0) {
+        InstallUninstallJava
+        WaitForUserInputToConitnue
     }
 }
 Main
